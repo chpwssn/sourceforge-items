8ab005bf660844f445f3e83a8a40248a60c3ab9a
Jesse Weinstein <jesse@wefu.org>
2015-06-06 08:06:26 -0700

    Basic segmentation for cache

--- rsync-disco/apiscrape.py
+++ rsync-disco/apiscrape.py
@@ -60,7 +60,8 @@ class sourceforge:
                 
     def load(self, path, page=1, limit=100):
         urlpath=self.project+("/"+path if path else "")
-        logpath=options.jsonlogdir+urlpath.replace('/','_')+".json"
+        assert urlpath[:2].isalnum()
+        logpath=options.jsonlogdir+urlpath[:2].lower()+"/"+urlpath.replace('/','_')+".json"
         url = "http://sourceforge.net/rest/p/%s?page=%d&limit=%d" % (urlpath, page, limit)
         if options.ignorelocal:
             print "Ignoring any caching, checking online for "+url
