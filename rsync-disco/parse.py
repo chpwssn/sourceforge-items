8b881867a8f9e1156cacd59d3d0e8cdcfa9755a1
Jesse Weinstein <jesse@wefu.org>
2015-06-10 21:37:01 -0700

    A few minor tweaks to parse.py

--- rsync-disco/parse.py
+++ rsync-disco/parse.py
@@ -23,11 +23,11 @@ with open(options.outfile,'w') as outfile:
                 print project
                 print mountpoint
                 print rsync
-                response = os.popen("rsync -av --stats --dry-run "+rsync[0]+" foo").read()
+                response = os.popen("rsync -a --stats --dry-run "+rsync[0]+" foo").read()
                 numfiles = re.findall('Number of files: ([0-9]+)', response)
                 filesize = re.findall('Total file size: ([^\n]+)', response)
                 print filesize
-                line =  project[0]+","+mountpoint[0]+","+rsync[0]+","+numfiles[0]+",\""+filesize[0]+"\""
+                line =  ','.join([project[0], mountpoint[0], rsync[0], numfiles[0], "\""+filesize[0]+"\""])
                 print line
                 outfile.write(line+"\n")
             else:
